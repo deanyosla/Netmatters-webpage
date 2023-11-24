1dfa3f34dbac9d00dcf7065901c1bda63e45be8b
deanyosla <146840740+deanyosla@users.noreply.github.com>
2023-11-24 14:05:48 +0000

    Attempted working on hidden sidebar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
-// const menu_nav = document.querySelector('.mobile-nav');
+const menu_nav = document.querySelector('#sideMenu');
+
 hamburger.addEventListener('click', function () {
     hamburger.classList.add('hamburger--spin');
     hamburger.classList.toggle("is-active");
-    menu_nav.classList.toggle("is-active");
+    menu_nav.classList.toggle('is-active');
 });
+
