090829fc201d0d118aac29a0dfc666d36b5caed8
deanyosla <146840740+deanyosla@users.noreply.github.com>
2023-11-27 17:43:38 +0000

    finished sticky header

--- js/main.js
+++ js/main.js
@@ -1,37 +1,54 @@
 
-const hamburger = document.querySelector('.hamburger');
-const menu_nav = document.querySelector('#sideMenu');
-const closeSideMenu = document.getElementById('close-side-menu');
-const body = document.getElementsByTagName('body');
-const html = document.getElementsByTagName('html');
+$(document).ready(function() {
+        const $hamburger = $('.hamburger');
+        const $menuNav = $('#sideMenu');
+        const $closeSideMenu = $('#close-side-menu');
+        const $body = $('body');
+        const $html = $('html');
+    
+        function toggleSideMenu() {
+            $hamburger.addClass('hamburger--spin');
+            $hamburger.toggleClass('is-active');
+            $menuNav.toggleClass('hidden-sidebar');
+            $body.toggleClass('side-menu-active');
+            $closeSideMenu.toggleClass('side-menu-active');
+            $body.toggleClass('stop-scroll');
+            $html.toggleClass('stop-scroll');
+        }
+    
+        $hamburger.click(toggleSideMenu);
+        $closeSideMenu.click(toggleSideMenu);
+    });
 
-function toggleSideMenu() {
-        hamburger.classList.add('hamburger--spin');
-        hamburger.classList.toggle("is-active");
-        menu_nav.classList.toggle('hidden-sidebar');
-        body[0].classList.toggle('side-menu-active');
-        closeSideMenu.classList.toggle('side-menu-active');
-        body[0].classList.toggle('stop-scroll');
-        html[0].classList.toggle('stop-scroll');        
+
+
+const head = $('.stickyH');
+const mainHeader = $('.header'); 
+const headerHeight = mainHeader.height() + 40;
+let lastScrollTop = 0;
+
+window.onscroll = function(e) {
+    const currentScrollTop = window.scrollY;
+    const isScrollingDown = currentScrollTop > lastScrollTop;
+
+    onScroll(isScrollingDown);
+    lastScrollTop = currentScrollTop;
+};
+
+function onScroll(isScrollingDown) {
+    let scroll = window.scrollY;
+
+    if (isScrollingDown && scroll > headerHeight) {
+        removeSticky();
+    } else {
+        addSticky();
+    }
 }
 
-hamburger.addEventListener('click', toggleSideMenu);
-closeSideMenu.addEventListener('click', toggleSideMenu);
+function addSticky() {
+    head.removeClass('sticky');
+}
 
-const nav = document.querySelector(".nav");
-  let lastScrollY = window.scrollY;
-
-  window.addEventListener("scroll", () => {
-    if (lastScrollY < window.scrollY) {
-      nav.classList.add("nav--hidden");
-    } else {
-      nav.classList.remove("nav--hidden");
-    }
-
-    lastScrollY = window.scrollY;
-    if (window.scrollY <= 0 ) {
-
-        nav.classList.remove('nav--hidden');
-    }
-
-  });
+function removeSticky() {
+    head.addClass('sticky');
+}
