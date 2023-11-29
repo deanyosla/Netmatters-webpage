3211bb41753fdd0c1edc042f4544ea547b2cacca
deanyosla <146840740+deanyosla@users.noreply.github.com>
2023-11-29 20:18:50 +0000

    issue with bg overlay

--- js/main.js
+++ js/main.js
@@ -53,50 +53,58 @@ function removeSticky() {
     head.addClass('sticky');
 }
 
-//  Set up to use local storage
+// Set up to use local storage
 const storageType = localStorage;
 const consentPropertyName = 'jdc_consent';
+
 // Check if the user should see the consent popup
-const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const showPopup = () => !storageType.getItem(consentPropertyName);
+
 // Save user's consent to local storage
 const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
 // Get references to HTML elements
 const body = document.getElementsByTagName('body');
 const html = document.getElementsByTagName('html');
 const consentParent = document.querySelector('.consent-div');
-let consentPopup;
-let background;
-// Run this code when the webpage finishes loading
-window.onload = () => {
-// Function to run when the user clicks "Accept"
+
+// Run this code when the DOM content has fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Function to run when the user clicks "Accept"
   const acceptFn = event => {
-      saveToStorage(storageType);
-      background.classList.add('bg-hidden');
-      consentParent.classList.add('hide');
-      body[0].classList.remove('stop-scroll');
-      html[0].classList.remove('stop-scroll');
+    saveToStorage(storageType);
+    background.classList.add('bg-hidden');
+    consentParent.classList.add('hide');
+    body[0].classList.remove('stop-scroll');
+    html[0].classList.remove('stop-scroll');
   };
-// Get references to HTML elements inside the page
+
+  // Get references to HTML elements inside the page
   consentPopup = document.getElementById('consent-popup');
   const acceptBtn = document.getElementById('accept');
   background = document.getElementById('bground');
+
   // Set up "Accept" button to run the function when clicked
   acceptBtn.addEventListener('click', acceptFn);
- // If the user hasn't given consent, show the popup when the page loads
-  if (shouldShowPopup(storageType)) {
-          background.classList.remove('bg-hidden');
-          consentParent.classList.remove('hide');
-          body[0].classList.add('stop-scroll');
-          html[0].classList.add('stop-scroll');
+
+  // If the user hasn't given consent, show the popup when the page loads
+  if (showPopup(storageType)) {
+    background.classList.remove('bg-hidden');
+    consentParent.classList.remove('hide');
+    body[0].classList.add('stop-scroll');
+    html[0].classList.add('stop-scroll');
   }
 
-};
-// Get reference to the consent button
-const consentButton = document.getElementById('consent-btn');
-// Set up the consent button to toggle visibility and scrolling when clicked
-consentButton.addEventListener('click', () => {
-  background.classList.toggle('bg-hidden');
-  consentParent.classList.toggle('hide');
-  body[0].classList.toggle('stop-scroll');
-  html[0].classList.toggle('stop-scroll');
+  // Get reference to the consent button
+  const consentButton = document.getElementById('consent-btn');
+
+  // Set up the consent button to toggle visibility and scrolling when clicked
+  consentButton.addEventListener('click', () => {
+    background.classList.toggle('bg-hidden');
+    consentParent.classList.toggle('hide');
+    body[0].classList.toggle('stop-scroll');
+    html[0].classList.toggle('stop-scroll');
+  });
+
 });
