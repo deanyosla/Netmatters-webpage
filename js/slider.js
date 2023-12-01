a4b59b86749fa835b5343901c82912771040e257
deanyosla <146840740+deanyosla@users.noreply.github.com>
2023-12-01 09:48:09 +0000

    slick for banners

--- js/slider.js
+++ js/slider.js
@@ -7,3 +7,60 @@
         pauseOnHover: false,
         pauseOnFocus: false,
     });
+
+    $(".work-showcase").slick({
+        // control the width of the images in the slider
+        variableWidth: true,
+        autoplay: true,
+        pauseOnHover: false,
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplaySpeed: 4000,
+        cssEase: "ease-in-out",
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false,
+              arrows: false,
+            },
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false,
+              arrows: false,
+            },
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false,
+              arrows: false,
+            },
+          },
+          {
+            breakpoint: 400,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false,
+              arrows: false,
+            },
+          },
+        ],
+      });
